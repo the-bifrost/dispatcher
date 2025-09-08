95f6c71e1ed422c7c078b6c2036d97bb771d346a
thalesgmartins <thalesdiniz30@gmail.com>
2025-09-08 09:34:48 -0300

    Criado um factory para processar e validar novos dispositivos

--- utils/device_factory.py
+++ utils/device_factory.py
@@ -31,33 +31,3 @@ def create_device(device_data: Dict[str, Any]) -> Device | None:
         logger.warning("Dados de dispositivo inválidos foram ignorados: %s", device_data)
         return None
     
-
-if __name__ == "__main__":
-    
-    espnow_data = {
-        "protocol":"espnow",
-        "device_type":"test",
-        "address":"AA:BB:CC:DD"
-    }
-
-    # Teste com dados válidos
-    esp_device = create_device(espnow_data)
-    if esp_device:
-        print(f"Sucesso! Objeto criado: {esp_device}")
-        print(f"Tipo do objeto: {type(esp_device)}")
-    else:
-        print("Falha ao criar o dispositivo LoRa.")
-
-    # Teste com dados inválidos
-    invalid_mqtt_data = {
-        "protocol":"mqtt",
-        "device_type":"test",
-        "topic":"sensors/mqtt/device"
-    }
-
-    mqtt_device = create_device(invalid_mqtt_data)
-    if esp_device:
-        print(f"Sucesso! Objeto criado: {esp_device}")
-        print(f"Tipo do objeto: {type(esp_device)}")
-    else:
-        print("Falha esperada. A função retornou None para o dispositivo inválido.")
