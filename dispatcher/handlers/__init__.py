b5b049671a753378ba3c05d0c8d59dd46c213235
thalesgmartins <thalesdiniz30@gmail.com>
2025-09-23 20:43:44 -0300

    adicionando novos protocolos no __init__

--- dispatcher/handlers/__init__.py
+++ dispatcher/handlers/__init__.py
@@ -1,4 +1,11 @@
-from .base_handler   import BaseHandler
+from .base_handler import BaseHandler
+from .base_serial_handler import BaseSerialHandler
 from .espnow_handler import EspNowHandler
+from .lora_handler import LoRaHandler
 
-__all__ = ["BaseHandler", "EspNowHandler"]
+__all__ = [
+    "BaseHandler",
+    "BaseSerialHandler",
+    "EspNowHandler",
+    "LoRaHandler"
+]
