08055776b41531bb88bdacb9f5d72c449dc8b740
thalesgmartins <thalesdiniz30@gmail.com>
2025-10-01 13:48:44 -0300

    movendo arquivos para dentro de dispatcher/

--- protocols/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base_handler   import BaseHandler
-from .espnow_handler import EspNowHandler
-from .mqtt_handler   import MqttHandler
-from .lora_handler   import LoraHandler
-
-__all__ = ["BaseHandler", "EspNowHandler", "MqttHandler", "LoraHandler"]
