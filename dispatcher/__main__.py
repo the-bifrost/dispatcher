a24e46b519be5320b7eddb077f9369b44edea09c
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-31 11:41:23 -0300

    impota libs de configuração, carrega configurações  do dispatcher, configura logs

--- dispatcher/__main__.py
+++ dispatcher/__main__.py
@@ -3,28 +3,35 @@
 import argparse
 import asyncio
 import logging
+import logging.config
 import sys
-
 from pathlib import Path
 
-from dispatcher import core
+from settings import load_config_file, load_configuration, Settings
+from start import start
 
-
+_LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+
+
+def setup_logging(path: Path):
+    """Carrega a configuração dos logs do sistema."""
+    logger_config = load_config_file(path)
+    logging.config.dictConfig(logger_config)
 
 
 def ensure_config_path(config_dir: Path) -> None:
     """Valida a existência do diretório de configuração."""
 
     if not config_dir.is_dir():
-        logging.warning("O diretório de configuração '%s' não foi encontrado", config_dir)
+        _LOGGER.warning("O diretório de configuração '%s' não foi encontrado", config_dir)
 
         # Se não existir tenta criar o diretório e seus pais, 
         # sem erros, caso já existam.
         try:
             config_dir.mkdir(parents=True, exist_ok=True)
         except OSError as e:
-            logging.error("Não foi possível criar o diretório de configuração '%s': %s", config_dir, e)
+            _LOGGER.error("Não foi possível criar o diretório de configuração '%s': %s", config_dir, e)
             sys.exit(1)
 
 
@@ -53,20 +60,25 @@ def main() -> int:
     config_dir = Path(args.config).resolve()
     ensure_config_path(config_dir)
 
-    logging.info("Usando diretório de configuração: %s", config_dir)
+    _LOGGER.info("Usando diretório de configuração: %s", config_dir)
+    settings = load_configuration(config_dir / "config.yaml")
 
+    _LOGGER.info("Configurando LOGGER de eventos")
+    setup_logging(config_dir / settings.PATH_LOGGER_CONFIG)
+
+    # Inicia a Dispatcher
     try:
         #asyncio.run(core.start(config_path=config_dir))
         pass
 
     except KeyboardInterrupt:
-        logging.info("Recebido comando de encerramento (CTRL + C).")
+        _LOGGER.info("Recebido comando de encerramento (CTRL + C).")
 
     except Exception as e:
-        logging.exception(f"Erro fatal durante a execução: {e}")
+        _LOGGER.exception("Erro fatal durante a execução: %s", e)
         return 1
 
-    logging.info("Dispatcher encerrado com sucesso.")
+    _LOGGER.info("Dispatcher encerrado com sucesso.")
     return 0
 
 
