7324f6bab66572f1475ea38c0d191c34443c8acc
thalesgmartins <thalesdiniz30@gmail.com>
2026-01-14 00:00:57 -0300

    add protocolo espnow

---
+++ dispatcher/protocols/espnow.py
@@ -0,0 +1,47 @@
+"""Protocolo EspNow que herda de BaseSerialProtocol."""
+import asyncio
+import logging
+
+import serial_asyncio
+from pydantic import BaseModel
+
+from protocols.serial import BaseSerialProtocol
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class EspNowConfig(BaseModel):
+    """Configs padrão do protocolo EspNow."""
+    port: str
+    baudrate: int = 9600
+
+
+class EspNowProtocol(BaseSerialProtocol):
+    def __init__(self):
+        super().__init__(protocol_name=__name__)
+
+
+async def setup_protocol(raw_config: dict):
+    """Faz o setup assíncrono do EspNow."""
+    
+    # Carrega configurações
+    try:
+        config = EspNowConfig(**raw_config)
+    except (TypeError, ValueError) as e:
+        _LOGGER.error(f"Erro de validação [{type(e).__name__}]: {e.__cause__}")
+        return
+    
+    # Pega o loop atual
+    loop = asyncio.get_event_loop()
+
+    _LOGGER.info("Iniciando EspNow na porta %s a %sbps", config.port, config.baudrate)
+
+    transport, protocol = await serial_asyncio.create_serial_connection(
+        loop,
+        LoraProtocol,
+        config.port,
+        config.baudrate
+    )
+
+    return protocol
