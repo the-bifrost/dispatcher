a36f431fd5f892b7921ae0c13157accf133343e7
thalesgmartins <thalesdiniz30@gmail.com>
2026-01-15 11:11:48 -0300

    add eventos

--- dispatcher/utils/const.py
+++ dispatcher/utils/const.py
@@ -3,14 +3,25 @@ from enum import Enum
 
 # eventos de telemetria
 # sensor.reading_received, temperature.changed
+class ProtocolEvent(Enum):
+    MESSAGE_RECEIVED = "protocol.message_received"
+
+class DeviceEvent(Enum):
+    MESSAGE_VALIDATED = "device.message_validated"
+    UNKNOWN = "device.unknown"
+    REGISTER_REQUEST = "device.register_request"
+    CONNECTED = "device.connected"
+    DISCONNECTED = "device.disconnected"
 
 # eventos de sistema
-# component.loaded, system.startup, device.connected, device.disconnected
+# component.loaded, system.startup
+class SystemEvent(Enum):
+    pass
 
 # eventos de comando
 # light.turn_on, message.send
 
-# Atual
+# Legacy
 class EventState(Enum):
     STATE_CHANGED = "state_changed"
     SEND_TO = "send_to_"
