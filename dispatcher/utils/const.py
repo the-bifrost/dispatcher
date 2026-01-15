9d6c06af3e89f93853ad6bf30a86c0b07f6283d2
thalesgmartins <thalesdiniz30@gmail.com>
2026-01-15 00:03:40 -0300

    add placeholder para futuros tipos de evento

--- dispatcher/utils/const.py
+++ dispatcher/utils/const.py
@@ -1,5 +1,16 @@
+"""Eventos e Variáveis constantes da Bifrost."""
 from enum import Enum
 
+# eventos de telemetria
+# sensor.reading_received, temperature.changed
+
+# eventos de sistema
+# component.loaded, system.startup, device.connected, device.disconnected
+
+# eventos de comando
+# light.turn_on, message.send
+
+# Atual
 class EventState(Enum):
     STATE_CHANGED = "state_changed"
     SEND_TO = "send_to_"
