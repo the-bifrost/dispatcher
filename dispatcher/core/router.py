146de300b43ac6040d51556a14adb25821ddca01
thalesgmartins <thalesdiniz30@gmail.com>
2026-01-21 17:26:34 -0300

    roteamento inteligente de mensagem, sem cadastro de dispsitivos

--- dispatcher/core/router.py
+++ dispatcher/core/router.py
@@ -2,8 +2,10 @@
 
 import logging
 
+from core.device_registry import DeviceRegistry
 from core.event_bus import event_bus
 from core.events import Events
+from utils.device import Device
 from utils.envelope import Envelope
 
 
@@ -11,30 +13,54 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Router:
+    def __init__(self, registry: DeviceRegistry):
         _LOGGER.info("Iniciando Router de Mensagens.")
 
+        self.registry = registry
 
         # O Router só ouve mensagens que já foram aprovadas pelo Registry
         event_bus.subscribe(Events.Device.VALIDATED, self.route_message)
 
         _LOGGER.info("Sucesso ao iniciar o Router!")
+
     async def route_message(self, data: dict):
         """Recebe um dicionário contendo o envelope e os dados do dispositivo."""
 
         envelope: Envelope | None = data.get("envelope")
+        device: Device | None = data.get("device")
 
-        if envelope:
-            target_protocol = envelope.protocol
+        if not envelope:
+            _LOGGER.warning("Recebeu um envelope inválido")
+            return
+        
+        if not device:
+            _LOGGER.warning("Recebeu um dispositivo inválido")
+            return
+        
+        
+        device_destino: Device | None = await self.registry.get_device(envelope.dst)
 
-            _LOGGER.info("Roteando mensagem de '%s' para protocolo '%s'", envelope.src, target_protocol)
+        if not device_destino:
+            _LOGGER.debug("Dispositivo destino não cadastrado.")
+            return
 
-            # Dispara o evento que os protocolos de SAÍDA estão ouvindo
-            # Ex: send_to_mqtt, send_to_lora
-            # os protocolos devem estar ouvindo "send_to_" + protocol_name
 
-            event_topic = f"{Events.Protocol.SEND_PREFIX}{target_protocol}"
-            await event_bus.publish(event_topic, envelope)
+        if envelope.type == "state":
+            await self.route_state_message(envelope, device_destino)
 
-        else:
-            _LOGGER.warning("Recebeu um envelope inválido: %s")
+
+    async def route_state_message(self, envelope: Envelope, device_destino: Device):
+        """
+        Dispara o evento de mudança de estado no protocolo correto.
+
+        Os protocolos devem estar ouvindo "protocol.send_to." + protocol_name 
+        Ex: protocol.send_to.mqtt, protocol.send_to.lora
+        """
+        target_protocol = device_destino.protocol
+        event_topic = f"{Events.Protocol.SEND_PREFIX}{target_protocol}"
+
+        _LOGGER.info("Roteando mensagem de '%s' para protocolo '%s'", envelope.src, target_protocol)
+        await event_bus.publish(event_topic, envelope)
+
+
 
