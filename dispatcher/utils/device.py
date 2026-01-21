7ef93c44a7ed40b3d10f47349542553da25b7596
thalesgmartins <thalesdiniz30@gmail.com>
2026-01-21 16:46:29 -0300

    add modelo base para dispositivos

---
+++ dispatcher/utils/device.py
@@ -0,0 +1,22 @@
+"""Padrão de Dispositivos da Bifrost."""
+
+import logging
+from typing import Any, Dict, Optional
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Device(BaseModel):
+    """Modelo Base para Dispositivos da Bifrost."""
+    id: str
+    protocol: str
+    token: Optional[str] = None
+    config: Dict[str, Any] = Field(default_factory=dict)
+    created_at: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
