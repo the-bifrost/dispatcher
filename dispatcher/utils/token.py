34164fc68adef5bbe26eea30e5b4a74bb18bf837
thalesgmartins <thalesdiniz30@gmail.com>
2026-02-04 16:50:19 -0300

    passando script teste para funções utilitárias

--- dispatcher/utils/token.py
+++ dispatcher/utils/token.py
@@ -9,3 +9,17 @@ import bcrypt
 _LOGGER = logging.getLogger(__name__)
 
 
+def generate_token(nbytes:int = 16) -> bytes:
+    """Gera um token para autenticação dos dispositivos."""
+    return secrets.token_bytes(nbytes=nbytes)
+
+def hash_token(bytes_token: bytes) -> bytes:
+    """Gera o hash para salvar no banco."""
+    return bcrypt.hashpw(bytes_token.hex().encode(), bcrypt.gensalt())
+
+def verify_token(bytes_token: bytes, hashed_token: bytes) -> bool:
+    """Compara um token e uma hash de token. Retorna True se iguais."""
+    if bcrypt.checkpw(bytes_token, hashed_token):
+        return True
+    
+    return False
