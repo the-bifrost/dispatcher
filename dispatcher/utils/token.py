528eb2bff600b54839d4ad5226a63be2bfa86f98
thalesgmartins <thalesdiniz30@gmail.com>
2026-02-04 17:19:38 -0300

    recebe uma string e devolve o hash em bytes

--- dispatcher/utils/token.py
+++ dispatcher/utils/token.py
@@ -13,9 +13,9 @@ def generate_token(nbytes:int = 16) -> str:
     """Gera um token para autenticação dos dispositivos e retorna em string hexadecimal."""
     return secrets.token_bytes(nbytes).hex()
 
-def hash_token(bytes_token: bytes) -> bytes:
+def hash_token(bytes_token: str) -> bytes:
     """Gera o hash para salvar no banco."""
-    return bcrypt.hashpw(bytes_token.hex().encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(bytes_token.encode(), bcrypt.gensalt())
 
 def verify_token(bytes_token: bytes, hashed_token: bytes) -> bool:
     """Compara um token e uma hash de token. Retorna True se iguais."""
